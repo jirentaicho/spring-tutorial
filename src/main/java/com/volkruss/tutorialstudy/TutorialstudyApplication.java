27a8ba5831eaed3bac1763cde69105b854046960
jiren <yujienjoyfever@yahoo.co.jp>
2022-10-11 08:44:57 +0900

    first commit

---
+++ src/main/java/com/volkruss/tutorialstudy/TutorialstudyApplication.java
@@ -0,0 +1,13 @@
+package com.volkruss.tutorialstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volkruss/tutorialstudy/TutorialstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkruss.tutorialstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
