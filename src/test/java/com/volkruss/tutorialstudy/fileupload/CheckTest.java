1fbc80a841d5faba967da0e7b25c67997398407d
jiren <yujienjoyfever@yahoo.co.jp>
2022-10-14 15:44:52 +0900

    d

---
+++ src/test/java/com/volkruss/tutorialstudy/fileupload/CheckTest.java
@@ -0,0 +1,24 @@
+package com.volkruss.tutorialstudy.fileupload;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CheckTest {
+    
+}
